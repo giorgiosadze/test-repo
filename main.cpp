6b78d687289dd956cba3d65af51aeaa9b9b3c46c
giorgiosadze <112907508+giorgiosadze@users.noreply.github.com>
2022-09-22 19:35:22 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-
+#include <ranges>
+#include <random>
 int main()
 {
-  std::cout << "test" << std::endl;
+  
+  std::random_device r;
+  std::vector<int> v {5,7,334,37};
+  std::ranges::sort(v,r);
+  
+  std::ranges::for_each(v, [](auto e) { std::cout << e << std::endl; });
+  
   return 0;
 }
