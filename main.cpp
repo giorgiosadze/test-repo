1c1c472b5f9870c84968ee00ee9d7f0930c4f117
giorgiosadze <112907508+giorgiosadze@users.noreply.github.com>
2022-09-22 19:43:00 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,8 @@
 #include <random>
 int main()
 {
-  
-  std::random_device r;
   std::vector<int> v {5,7,334,37};
-  std::sort(v.begin(), v.end(),r);
+  std::sort(v.begin(), v.end());
   
   std::for_each(v.begin(), v.end(), [](auto e) { 
     std::cout << e << std::endl; 
