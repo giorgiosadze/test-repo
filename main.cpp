902f8d06ddc647087625f51ff933823c165f9f80
giorgiosadze <112907508+giorgiosadze@users.noreply.github.com>
2022-10-08 22:22:41 -0400

    back to fixed

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <random>
 int main()
 {
-  std::vector<int> v {5,7,334,37}
+  std::vector<int> v {5,7,334,37};
   std::sort(v.begin(), v.end());
   
   std::for_each(v.begin(), v.end(), [](auto e) { 
