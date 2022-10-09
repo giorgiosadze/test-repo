983fef1207a5c092f6714dbe7373e9c8facdc120
giorgiosadze <112907508+giorgiosadze@users.noreply.github.com>
2022-10-09 02:23:10 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,7 @@ int main()
   int a = 234;
   std::vector<int> v {5,7,334,37};
   std::sort(v.begin(), v.end());
-  for(;;) {}
-  for(;;) {}
+
   std::for_each(v.begin(), v.end()), [](auto e) { 
     std::cout << e << std::endl; 
   });
