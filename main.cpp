654df7a50f8ef7a3614580a4e233fa358a480c4f
giorgiosadze <112907508+giorgiosadze@users.noreply.github.com>
2022-10-09 02:26:04 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <algorithm>
 #include <random>
-int main()
+int main() -> int
 {
   int a = 234;
   std::vector<int> v {5,7,334,37};
   std::sort(v.begin(), v.end());
-
   std::for_each(v.begin(), v.end()), [](auto e) { 
     std::cout << e << std::endl; 
   });
